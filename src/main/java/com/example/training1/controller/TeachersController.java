d427c78e87239149a49c89fcd6acf9133c524624
Zuzanna Stolc <zuzanna.stolc03@gmail.com>
2023-06-11 14:32:36 +0200

    Added a few more methods and exception handling to displaying bad data in Grades Controller

--- src/main/java/com/example/training1/controller/TeachersController.java
+++ src/main/java/com/example/training1/controller/TeachersController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
- @RequestMapping("/teachers")
+@RequestMapping("/teachers")
 public class TeachersController {
     private final Teacher englishTeacher, mathsTeacher, physicalEducationTeacher;
 
@@ -16,29 +18,34 @@ public class TeachersController {
     private String subject;
 
     TeachersController(@Qualifier("english") Teacher theEnglishTeacher, @Qualifier("maths") Teacher theMathsTeacher,
-                       @Qualifier("physicalEducation") Teacher thePhysicalEducationTeacher){
+                       @Qualifier("physicalEducation") Teacher thePhysicalEducationTeacher) {
         englishTeacher = theEnglishTeacher;
         mathsTeacher = theMathsTeacher;
         physicalEducationTeacher = thePhysicalEducationTeacher;
     }
+
     @GetMapping(value = {"", "/"})
-    public String teachers(){
+    public String teachers() {
         return "Welcome to teachers section!";
     }
+
     @GetMapping("/maths")
-    public String mathsTeacher(){
+    public String mathsTeacher() {
         return mathsTeacher.pleaseLearn();
     }
+
     @GetMapping("/english")
-    public String englishTeacher(){
+    public String englishTeacher() {
         return englishTeacher.pleaseLearn();
     }
+
     @GetMapping("/physicalEducation")
-    public String physicalEducationTeacher(){
+    public String physicalEducationTeacher() {
         return physicalEducationTeacher.pleaseLearn();
     }
+
     @GetMapping("/subject")
-    public String theTeacher(){
+    public String theTeacher() {
         return "There is no homework to do in " + subject;
     }
 }
